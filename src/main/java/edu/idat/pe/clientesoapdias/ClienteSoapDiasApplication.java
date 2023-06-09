5a54066c7f07403acfd6a939ddba889ec5bb5482
Anticona23 <anticonacr@gmail.com>
2023-06-08 23:44:02 -0500

    Ejercicio 1

---
+++ src/main/java/edu/idat/pe/clientesoapdias/ClienteSoapDiasApplication.java
@@ -0,0 +1,13 @@
+package edu.idat.pe.clientesoapdias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteSoapDiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteSoapDiasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/idat/pe/clientesoapdias/ClienteSoapDiasApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.idat.pe.clientesoapdias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteSoapDiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
